30909ac1678f5f379a82c333ee4102dcea68021d
Maximo Bautista <max.bautista97@gmail.com>
2019-01-31 22:43:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   end
   
   def self.find_by_name(song_title)
-    self.all.take() {|song| song.name == song_title }
+    self.all.select{|song| song.name == song_title }
   end
   
   def self.find_or_create_by_name(name)
