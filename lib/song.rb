95e4ecf83704cc6106fc7f661ed8734f3955508c
Maximo Bautista <max.bautista97@gmail.com>
2019-01-31 23:23:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,8 @@ class Song
   
   def self.new_from_filename(song_title)
     song_details = song_title.split(/[-.]/)
-    song = self.create_by_name(song_details[1].strip)
+    song = self.new
+    song.name = song_details[1].strip)
     song.artist_name = song_details[0].strip
 
   end
